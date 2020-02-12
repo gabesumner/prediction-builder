985509fca35a8e7bbad4ae59690f06aa5b83cafb
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-02-12 13:26:16 -0800

    First commit

---
+++ orgInit.sh
@@ -0,0 +1,6 @@
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o predictionbuilder.demo
+sfdx force:source:push
+sfdx force:user:permset:assign -n PredictionBuilder
+sfdx shane:data:tree:import -p data/plan.json -d data/
+sfdx force:user:password:generate
+sfdx force:org:open
