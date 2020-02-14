d642318b4c6cdbc0961dfb7fa1cef2e82220b894
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-02-13 16:13:47 -0800

    Shortened username

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o predictionbuilder.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o prediction.demo
 sfdx force:source:push
 sfdx force:user:permset:assign -n PredictionBuilder
 sfdx shane:data:tree:import -p data/plan.json -d data/
