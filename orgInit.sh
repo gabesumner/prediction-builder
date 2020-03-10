7e066684972db36610ae17ab933a69d83c6ca9b1
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-10 15:36:34 -0700

    Switched to standard org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o prediction.demo
+#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o prediction.demo
+sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n PredictionBuilder
 sfdx shane:data:tree:import -p data/plan.json -d data/
